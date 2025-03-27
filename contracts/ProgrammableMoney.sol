e6abebc9ae4c944daaa7feb6f9392aa827cef41b
Yat Wai Wong <yat.wai.wong@rwth-aachen.de>
2025-03-27 11:12:01 +0800

    feature: ProgrmmableMoney.sol using embedded on-chain verifier.
    - Only contract owner can define spending conditions (this is limited by Privado's EmbeddedZKPVerifier.sol).
    - Only contract owner can mint token.
    - Proof request submission with hardhat tested (https://docs.privado.id/docs/verifier/on-chain-verification/set-zkp-request/#approach---b-setting-request-programmatically)

--- contracts/ProgrammableMoney.sol
+++ contracts/ProgrammableMoney.sol
@@ -1,14 +1,51 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.27;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+//import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
+//import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+//import "@openzeppelin/contracts/access/AccessControl.sol";
+//import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+//import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+//import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+//@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol';
+//import "@openzeppelin/contracts/access/Ownable.sol";
+//import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 import {PrimitiveTypeUtils} from '@iden3/contracts/lib/PrimitiveTypeUtils.sol';
 import {ICircuitValidator} from '@iden3/contracts/interfaces/ICircuitValidator.sol';
 import {EmbeddedZKPVerifier} from '@iden3/contracts/verifiers/EmbeddedZKPVerifier.sol';
 
-contract ProgrammableMoney is ERC1155, Ownable, ERC1155Burnable {
+contract ProgrammableMoney is /*Initializable, UUPSUpgradeable, OwnableUpgradeable, */ ERC1155Upgradeable, EmbeddedZKPVerifier {
+    // Mapping to track approved minters.
+    mapping(address => bool) public approvedMinters;
+    address public admin;
+
+    modifier onlyApprovedMinter() {
+        require(approvedMinters[msg.sender], "Not an approved minter");
+        _;
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Not the admin");
+        _;
+    }
+
+    /**
+     * @notice Add a new approved minter.
+     * @dev Only the admin can add new minters.
+     */
+    function addMinter(address minter) external onlyAdmin {
+        approvedMinters[minter] = true;
+    }
+
+    /**
+     * @notice Remove an approved minter.
+     * @dev Only the admin can remove minters.
+     */
+    function removeMinter(address minter) external onlyAdmin {
+        approvedMinters[minter] = false;
+    }
+
     // Change the request id after creating the proof request
     uint64 public constant TRANSFER_REQUEST_ID_SIG_VALIDATOR = 1;
     uint64 public constant TRANSFER_REQUEST_ID_MTP_VALIDATOR = 2;
@@ -25,12 +62,37 @@ contract ProgrammableMoney is ERC1155, Ownable, ERC1155Burnable {
     uint256 public constant fungible = 0;
     uint256 public constant nonFungible = 1;
 
-    //The Owner is supposed to be the 'controller', i.e. only owner can add or delete conditinos
-    constructor(address initialOwner) ERC1155("https://ipfs.io/ipfs/QmaP8dJtET7rr5198RCpUFrXFbGps3VpFyAsaczFQntkwH/{id}.json") Ownable(initialOwner) {
-        super.__EmbeddedZKPVerifier_init(_msgSender());
+/*
+    function customSetZKPRequest(uint64 requestID, string metadata, ICircuitValidator validator, bytes data){
+        IZKPVerifier.ZKPRequest request = IZKPVerifier.ZKPRequest(metadata, validator, data);
     }
+*/
+    //The Owner is supposed to be the 'controller', i.e. only owner can add or delete conditinos
+    //constructor() ERC1155("https://ipfs.io/ipfs/QmaP8dJtET7rr5198RCpUFrXFbGps3VpFyAsaczFQntkwH/{id}.json"){
+    //    super.__EmbeddedZKPVerifier_init(_msgSender());
+    //}
 
-    // Specify the 
+   // function initialize() initializer public{
+   //__ERC1155_init("https://ipfs.io/ipfs/QmaP8dJtET7rr5198RCpUFrXFbGps3VpFyAsaczFQntkwH/{id}.json");
+   //super.__EmbeddedZKPVerifier_init(_msgSender());
+   //__Ownable_init();
+   //__UUPSUpgradeable_init();
+//}
+
+
+    function initialize(string memory uri) public initializer {
+      //super.__EmbeddedZKPVerifier_init(_msgSender());
+      super.__ERC1155_init(uri);
+      super.__EmbeddedZKPVerifier_init(_msgSender());
+      admin = msg.sender;
+      approvedMinters[msg.sender] = true;
+   }
+
+    //constructor(address initialOwner) Ownable(initialOwner) {
+    //    //super.__EmbeddedZKPVerifier_init(_msgSender());
+    //}
+
+    // Specify the tx info
     struct txInfo {
         address from;
         address to;
@@ -54,55 +116,65 @@ contract ProgrammableMoney is ERC1155, Ownable, ERC1155Burnable {
      * @param data Extra info, can be "".
      */
     function setTxInfo(
-        uint256 requestID, 
+        uint64 requestID, 
         address from,
         address to,
         uint256 tokenID,
         uint256 amount,
-        bytes data) external {
-        require(balanceOf(msg.sender, tokenId) > 0, "Caller does not own the token");
-        requestID_txInfo[key] = txInfo(from, to, tokenID, amount, data);
+        bytes memory data) external {
+        require(balanceOf(msg.sender, tokenID) > 0, "Caller does not own the token");
+        requestID_txInfo[requestID] = txInfo(from, to, tokenID, amount, data);
     }
 
+    bool public is_verified;
+    event FunctionCalled(string message, bool value);
+    event FunctionCalledBeforeProofSubmit(string message, address value);
 
     function _beforeProofSubmit(
         uint64 /* requestId */,
         uint256[] memory inputs,
         ICircuitValidator validator
-        ) internal view override {
+        ) internal override{
         // check that challenge input is address of sender
         address addr = PrimitiveTypeUtils.uint256LEToAddress(
             inputs[validator.inputIndexOf('challenge')]
         );
+        emit FunctionCalledBeforeProofSubmit("prover's address = ", addr);
         // this is linking between msg.sender and
-        require(_msgSender() == addr, 'address in proof is not a sender address');
+        //require(_msgSender() == addr, 'address in proof is not a sender address');
         }
     function _afterProofSubmit(
         uint64 requestId,
         uint256[] memory inputs,
         ICircuitValidator validator
-        ) internal override {
+        ) internal override{
         if (
             // Check if a transaction info is set for the request id
             // Transaction only happens if the tx_info is set.
-            requestId_txInfo[requestId].from != address(0)
+            //requestID_txInfo[requestId].from != address(0)
+            true
         ) {
             // if proof is given for transfer request id ( mtp or sig ) we send tokens according to the tx_info of the request id.
-            info = requestId_txInfo[requestId];
-            address from = _txinfo.from;
-            address to = _txinfo.to;
-            uint256 tokenID = _txinfo.tokenID;
-            uint256 amount = _txinfo.amount;
-            bytes data = _txinfo.data;
-            safeTransferFrom(from, to, tokenID, amount, data, _msgSender(), requestId);
+            /*
+            txInfo memory info = requestID_txInfo[requestId];
+            address from = info.from;
+            address to = info.to;
+            uint256 tokenID = info.tokenID;
+            uint256 amount = info.amount;
+            bytes memory data = info.data;
+            */
+            is_verified = isProofVerified(_msgSender(), requestId);
+            emit FunctionCalled("is_verified = ", is_verified);
+
+            //customTransferFrom(from, to, tokenID, amount, data, _msgSender(), requestId);
         }
         else{
             revert("Error: transaction info is not set for the request id of the proof.");
         }
     }
 
-    function safeTransferFrom(address from, address to, uint256 id, uint256 value, bytes memory data, address prover_address, uint256 requestID)
-    public virtual override{
+    function customTransferFrom(address from, address to, uint256 id, uint256 value, bytes memory data, address prover_address, uint64 requestID)
+    public {
         require(
          isProofVerified(prover_address, requestID),
          'only identities who provided sig or mtp proof for transfer requests are allowed to receive tokens'
@@ -111,88 +183,23 @@ contract ProgrammableMoney is ERC1155, Ownable, ERC1155Burnable {
         // Call the original function for its intended behavior
         super.safeTransferFrom(from, to, id, value, data);
     }
-        function setURI(string memory newuri) public onlyOwner {
-        _setURI(newuri);
-    }
-
+    
+    /**
+     * @notice Mint new tokens.
+     * @dev Only approved minters can mint tokens.
+     */
     function mint(address account, uint256 id, uint256 amount, bytes memory data)
-        public
-        onlyOwner
+        external onlyApprovedMinter
     {
         _mint(account, id, amount, data);
     }
 
     function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        public
-        onlyOwner
+        external onlyApprovedMinter
     {
         _mintBatch(to, ids, amounts, data);
     }
     
-// ====================================================
-// The functions below are intended for off-chain verification.
-// Keep them as a backup and maybe useful later.
-// ====================================================
 
-    // Define operators of the conditions, as an enum for clarity.
-    enum Operator { GreaterThan, LessThan, Equal, NotEqual, OneOfValues, NoneOfValues}
-
-    // Structure to represent a spending condition.
-    struct Condition {
-        string parameter;  // e.g., "age"
-        Operator op;       // e.g., GreaterThan
-        uint256[] values; // e.g., 18
-    }
-
-    // Map a token id to an array of conditions.
-    mapping(uint256 => Condition[]) public tokenConditions;
-
-    // Add a condition to a token.
-    // Function caller must have the token.
-    function addCondition(
-        uint256 tokenId,
-        string memory parameter,
-        Operator op,
-        uint256[] memory values
-    ) external  {
-        require(balanceOf(msg.sender, tokenId) > 0, "Caller does not own the token");
-        tokenConditions[tokenId].push(Condition(parameter, op, values));
-    }
-
-    // Delete a condition by index from a token's conditions array.
-    // Function caller must have the token.
-    // This function swaps the condition with the last element and then removes it.
-    function deleteCondition(uint256 tokenId, uint256 index) external {
-        require(balanceOf(msg.sender, tokenId) > 0, "Caller does not own the token");
-        require(index < tokenConditions[tokenId].length, "Invalid index");
-        tokenConditions[tokenId][index] = tokenConditions[tokenId][tokenConditions[tokenId].length - 1];
-        tokenConditions[tokenId].pop();
-    }
-
-    // Public function returns the spending conditions of a token.
-    // Depending on the use cases, this function may not be public
-    function getConditions(uint256 tokenId) public view returns (Condition[] memory) {
-    return tokenConditions[tokenId];
-}
-    // Mapping to record if a credentials holder has met the conditions for a given token.
-    // conditionsMet[tokenId][credentials holder's address] == true means the credentials holder has met all conditions for that token.
-    mapping(uint256 => mapping(address => bool)) private _conditionsMet;
-
-    // Takes a token ID and an address, then return whether the address satisfy the spending conditions:
-    // Depending on the use cases, this function may not be public
-    function getConditionsMet(uint256 tokenId, address holder) public view returns (bool) {
-        return _conditionsMet[tokenId][holder];
-    }
-
-    // spending conditions met: only allow transfers when all spending conditions are met.
-    // the condiions check (verificaiton of ZKP) can be done on-chain or off-chain via Privado ID's JS-SDK.
-    mapping(uint256 => bool) public transfersAllowed;
-
-    // Function to toggle transfer permission of a token, after ZKP is verified successfully.
-    // Function caller must have that token.
-    function setTransfersAllowed(uint256 tokenId, bool allowed) external {
-    require(balanceOf(msg.sender, tokenId) > 0, "Caller does not own the token");
-       transfersAllowed[tokenId] = allowed;
-    }
 
 }
